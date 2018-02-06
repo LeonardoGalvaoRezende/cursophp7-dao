ffdaa73f4ff4ea96ed427b417547da9e2eb32edc
leonardo <lgr.rockandroll@hotmail.com>
2018-02-05 19:15:31 -0500

    iniciando o projeto

---
+++ class/Sql.php
@@ -0,0 +1,49 @@
+<?php 
+ /**
+  * summary
+  */
+ class Sql extends PDO
+ {
+     
+    //ATRIBUTOS
+
+ 	private $conn;
+
+ 	//METODOS
+
+ 	//Insere varios valores
+ 	private function setParams($statment,$parameters = array()) {
+
+ 		foreach ($parameters as $key => $value) {
+ 			$this->setParam($key,$value);
+ 		}
+ 	}
+ 	//Insere um valor
+ 	private function setParam($key,$value) {
+ 		$statment->bindParam($key,$value);
+ 	}
+ 	//Prepara e executa query
+ 	public function query($rawQuery,$params = array()) {
+
+ 		$stmt = $this->conn->prepare($rawQuery);
+ 		$this->setParams($stmt,$params);
+ 		$stmt->execute();
+ 		return $stmt;
+ 	}
+ 	//Select
+ 	public function select($rawQuery,$params = array()):array
+ 	 {
+ 		$stmt = $this->query($rawQuery,$params);
+ 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+ 	}
+
+ 	//METODOS ESPECIAIS
+
+ 	//Construtor
+     public function __construct()
+     {
+         $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","#3v3r357");
+     }
+ }
+
+ ?>
