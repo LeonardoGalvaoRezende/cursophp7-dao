064be4b8c321ace0dbbaae7973d6d6dc4343b5c0
leonardo <lgr.rockandroll@hotmail.com>
2018-02-07 19:17:17 -0500

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -10,7 +10,7 @@ class Usuario {
 	private $dtcadastro;
 
 	//METODOS
-
+	//Carrega um usuario pelo id
 	public function loadById($id) {
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
@@ -24,6 +24,37 @@ class Usuario {
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+	//Carrega lista de usuarios
+	public static function getList() {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin"); 
+	}
+	//Procura usuario
+	public static function search($login) {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+	//Login com usuario e senha e retorna informaçoes
+	public function login($login,$password) {
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+			':LOGIN'=>$login,
+			':PASSWORD'=>$password
+		));
+
+		if (count($results) > 0) {
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+			throw new Exception("Login e/ou Senha invalidos!");
 		}
 	}
 
--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 require_once("config.php");
 
 // $sql = new Sql();
-
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 // echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuario
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root->loadById(3);
+//Carrega lista de usuarios
+// $lista = Usuario::getList();//metodo estatico
+// echo json_encode($lista);
 
-echo $root;
+//Carrega lista de usuarios buscando pelo login
+// $search = Usuario::search("j");//metodo estatico
+// echo json_encode($search);
 
+//Carrega dados do usuario se senha e login forem corretos
+$usuario = new Usuario();
+$usuario->login("Root","12345");
+echo $usuario;
  ?>
